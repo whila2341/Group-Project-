f53ad7e867d22a904402cb33231aa761c4c729a1
Lauren <houseoflew@live.com>
2026-07-12 20:55:55 -0400

    feat: added features to promo_code schema

--- FinalProject/api/schemas/promo_codes.py
+++ FinalProject/api/schemas/promo_codes.py
@@ -6,4 +6,13 @@ class PromoCodeBase(BaseModel):
     discount: int
     expiration_date: datetime
 
+class PromoCodeCreate(PromoCodeBase):
+    pass
 
+
+class PromoCodeResponse(PromoCodeBase):
+    id: int
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
